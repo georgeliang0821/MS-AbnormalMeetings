9a247e5305de9eb5773cc6632b5e08a590bb2531
yhlu <eric890923@gmail.com>
2022-07-31 22:37:47 +0800

    Add store the jsonstring function to Azure Blob

--- GetCallRecords_Http/GetCallRecords_Http.cs
+++ GetCallRecords_Http/GetCallRecords_Http.cs
@@ -17,85 +17,82 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json.Nodes;
 using daemon_console;
+using System.Collections.Generic;
+using Azure.Storage.Blobs;
+using System.Text;
 using System.Net;
-using Newtonsoft.Json.Linq;
-
-//using GraphWebhooks.Helpers;
-//using GraphWebhooks.Models;
-//using GraphWebhooks.SignalR;
-//using Newtonsoft.Json;
-//using Newtonsoft.Json.Linq;
-//using System;
-//using System.Collections.Generic;
-//using System.Security.Claims;
-//using System.Threading.Tasks;
-//using System.Web.Mvc;
 
 namespace GetCallRecords_Http
 {
-    public static class GetCallRecords_Http
+
+    public class GetCallRecords_Http
     {
         [FunctionName("GetCallRecords_Http")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("Our GetCallRecords_Http is triggered.");
+            log.LogInformation("GetCallRecords_Http is triggered.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //log.LogInformation(requestBody);
 
-            log.LogInformation(requestBody);
-
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //////////////////// This is a validation process, so rutrun and stop program here! ////////////////////
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
             string query_validationToken = req.Query["validationToken"];
             if (query_validationToken != null)
             {
-                log.LogInformation("The validationToken is: ");
-                log.LogInformation(query_validationToken);
-
+                log.LogInformation("The validationToken is: " + query_validationToken);
                 return new OkObjectResult(query_validationToken);
             }
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            //return new OkResult();
-
-            string meetingID = "68dfa85b-3d18-44de-937e-a639d313c6ff";
+            SubscriptionData subscriptionData = JsonConvert.DeserializeObject<SubscriptionData>(requestBody);
+            string meetingID = subscriptionData.value[0].resourceData.id;
 
             IConfidentialClientApplication app;
             try
             {
-                log.LogInformation("Getting call records...");
+                log.LogInformation("Login to application...");
 
-                AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("loginsettings.json");
-          
+                AuthenticationConfig config = new AuthenticationConfig();
+
+                config.Instance = Environment.GetEnvironmentVariable("Instance");
+                config.ApiUrl = Environment.GetEnvironmentVariable("ApiUrl");
+                config.Tenant = Environment.GetEnvironmentVariable("Tenant");
+                config.ClientId = Environment.GetEnvironmentVariable("ClientId");
+                config.ClientSecret = Environment.GetEnvironmentVariable("ClientSecret");
+
                 app = GetAppAsync(config);
+                log.LogInformation("Success login.");
 
+                log.LogInformation("Getting call records...");
                 // With client credentials flows the scopes is ALWAYS of the shape "resource/.default", as the 
                 // application permissions need to be set statically (in the portal or by PowerShell), and then granted by
                 // a tenant administrator. 
                 string[] scopes = new string[] { $"{config.ApiUrl}.default" }; // Generates a scope -> "https://graph.microsoft.com/.default"
 
                 // Call MS graph using the Graph SDK
-                await CallMSGraphUsingGraphSDK(app, scopes, meetingID, log);
+                log.LogInformation("Running Function: CallMSGraphUsingGraphSDK");
+                CallRecord callrecord = await CallMSGraphUsingGraphSDK(app, scopes, meetingID, log);
 
-                // Call MS Graph REST API directly
-                // await CallMSGraph(config, app, scopes);
+                //log.LogInformation("Start time: " + callrecord.StartDateTime);
+                //log.LogInformation("End time: " + callrecord.EndDateTime);
+                string jsonString = System.Text.Json.JsonSerializer.Serialize(callrecord);
+                log.LogInformation("jsonString: " + jsonString);
 
-                AuthenticationResult result = null;
-                result = await app.AcquireTokenForClient(scopes).ExecuteAsync();
+                log.LogInformation("Writing file...");
+                await SaveToBlob(callrecord, log);
+                log.LogInformation("Success writing file: " + meetingID + ".json");
+                
+                //// Call MS Graph REST API directly
+                //log.LogInformation("Running Function: CallMSGraph");
+                //await CallMSGraph(config, app, scopes, meetingID);
 
-                var response = new
-                    {
-                        AccessToken = result.AccessToken,
-                        x = "hello",
-                        y = "world",
-
-                    };
-                    //if (query_validationToken != null)
-                    //{
-                    //    return new OkObjectResult(query_validationToken);
-                    //}
-
-                return new OkObjectResult(JsonConvert.SerializeObject(response));
+                return new OkObjectResult(JsonConvert.SerializeObject("Successfully run!"));
 
             }
             catch (Exception ex)
@@ -103,48 +100,95 @@ namespace GetCallRecords_Http
                 log.LogError(ex.Message);
             }
 
-            return new BadRequestResult();
-
-            //string responseMessage = "Token is token";
-            //return new OkObjectResult(responseMessage);
-
-            //if (req.QueryString["validationToken"] != null)
-            //{
-            //    var token = Request.QueryString["validationToken"];
-            //    return Content(token, "text/plain");
-            //}
+            return new BadRequestObjectResult("Something wrong happened!");
 
         }
 
+        private static async Task SaveToBlob(CallRecord callrecord, ILogger log)
+        {
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(callrecord);
+
+            string connectionString = Environment.GetEnvironmentVariable("BlobConnectionString");
+            string containerName = Environment.GetEnvironmentVariable("BlobContainerName");
+            BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
+
+            var blob = container.GetBlobClient(callrecord.Id + ".json");
+            using (MemoryStream mem = new MemoryStream())
+            {
+                // Write to stream
+                Byte[] info = new UTF8Encoding(true).GetBytes(jsonString);
+                mem.Write(info, 0, info.Length);
+
+                // Go back to beginning of stream
+                mem.Position = 0;
+
+                await blob.UploadAsync(mem, overwrite:true);
+
+                // TODO: Handle the BlobAlreadyExists code
+                //try
+                //{
+                //    await blob.UploadAsync(mem);
+                //    //await blob.UploadAsync(mem, overwrite:true);
+                //}
+                //catch (WebException ex)
+                //{
+                //    Console.WriteLine("Enter WebException");
+                //    if (ex.Status == WebExceptionStatus.ProtocolError)
+                //    {
+                //        var response = ex.Response as HttpWebResponse;
+                //        // Handling response.StatusCode == 409 here! 
+                //        if (response != null && (int)response.StatusCode == 409)
+                //        {
+                //            // If file exist: just overwrite it
+                //            log.LogInformation("Try to catch error 409; ErrorCode: BlobAlreadyExists");
+                //            await blob.UploadAsync(mem, overwrite: true);
+                //            log.LogInformation("Success! Catch BlobAlreadyExists using overwrite");
+                //        }
+                //        else
+                //        {
+                //            // no http status code available
+                //            log.LogError(ex.Message);
+                //        }
+                //    }
+                //    else
+                //    {
+                //        // no http status code available
+                //        log.LogError(ex.Message);
+                //    }
+                //}
+                //catch (Exception ex)
+                //{
+                //    Console.WriteLine("Enter Exception");
+                //    try
+                //    {
+                //        Console.WriteLine("Enter Exception Catch");
+                //        log.LogInformation("Enter Exception: " + ex.Message);
+                //        await blob.UploadAsync(mem, overwrite:true);
+                //        log.LogInformation("Success! Overcome the exception using overwrite.");
+                //    }
+                //    catch
+                //    {
+                //        Console.WriteLine("Enter Exception Catch");
+                //        log.LogError(ex.Message);
+                //    }
+                //}
+            }
+
+        }
 
         private static IConfidentialClientApplication GetAppAsync(AuthenticationConfig config)
         {
-
-            // You can run this sample using ClientSecret or Certificate. The code will differ only when instantiating the IConfidentialClientApplication
-            bool isUsingClientSecret = IsAppUsingClientSecret(config);
+            //// You can run this sample using ClientSecret or Certificate. The code will differ only when instantiating the IConfidentialClientApplication
+            //bool isUsingClientSecret = IsAppUsingClientSecret(config);
 
             // Even if this is a console application here, a daemon application is a confidential client application
             IConfidentialClientApplication app;
 
-            if (isUsingClientSecret)
-            {
-                // Even if this is a console application here, a daemon application is a confidential client application
-                app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
-                    .WithClientSecret(config.ClientSecret)
-                    .WithAuthority(new Uri(config.Authority))
-                    .Build();
-            }
-
-            else
-            {
-                ICertificateLoader certificateLoader = new DefaultCertificateLoader();
-                certificateLoader.LoadIfNeeded(config.Certificate);
-
-                app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
-                    .WithCertificate(config.Certificate.Certificate)
-                    .WithAuthority(new Uri(config.Authority))
-                    .Build();
-            }
+            // Even if this is a console application here, a daemon application is a confidential client application
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.ClientSecret)
+                .WithAuthority(new Uri(config.Authority))
+                .Build();
 
             app.AddInMemoryTokenCache();
 
@@ -158,7 +202,7 @@ namespace GetCallRecords_Http
         /// <param name="app"></param>
         /// <param name="scopes"></param>
         /// <returns></returns>
-        private static async Task CallMSGraph(AuthenticationConfig config, IConfidentialClientApplication app, string[] scopes)
+        private static async Task CallMSGraph(AuthenticationConfig config, IConfidentialClientApplication app, string[] scopes, string meetingID)
         {
             AuthenticationResult result = null;
             try
@@ -184,8 +228,12 @@ namespace GetCallRecords_Http
             {
                 var httpClient = new HttpClient();
                 var apiCaller = new ProtectedApiCallHelper(httpClient);
-                await apiCaller.CallWebApiAndProcessResultASync($"{config.ApiUrl}v1.0/users", result.AccessToken, Display);
-
+                //await apiCaller.CallWebApiAndProcessResultASync($"{config.ApiUrl}v1.0/users", result.AccessToken, Display);
+                Console.WriteLine("Running: await apiCaller.CallWebApiAndProcessResultASync");
+                await apiCaller.CallWebApiAndProcessResultASync(
+                    $"{config.ApiUrl}v1.0/communications/callRecords/{meetingID}",
+                    result.AccessToken, Display);
+                    //$"{config.ApiUrl}v1.0/communications/callRecords/{meetingID}?$expand=sessions($expand=segments)",
             }
         }
 
@@ -195,35 +243,26 @@ namespace GetCallRecords_Http
         /// <param name="app"></param>
         /// <param name="scopes"></param>
         /// <returns></returns>
-        private static async Task CallMSGraphUsingGraphSDK(IConfidentialClientApplication app, string[] scopes, string meetingID, ILogger log)
+        private static async Task<CallRecord> CallMSGraphUsingGraphSDK(IConfidentialClientApplication app, string[] scopes, string meetingID, ILogger log)
         {
             // Prepare an authenticated MS Graph SDK client
             GraphServiceClient graphServiceClient = GetAuthenticatedGraphClient(app, scopes);
 
-
-            // List<User> allUsers = new List<User>();
-
             try
             {
-
-                // IGraphServiceUsersCollectionPage users = await graphServiceClient.Users.Request().GetAsync();
-                // Console.WriteLine($"Found {users.Count()} users in the tenant"); 
-
                 CallRecord callrecord = await graphServiceClient.Communications.CallRecords[meetingID]
                     .Request()
+                    .Expand("sessions($expand=segments)")
                     .GetAsync();
 
-                log.LogInformation("Start time: " + callrecord.StartDateTime);
-                log.LogInformation("End time: " + callrecord.EndDateTime);
-
+                return callrecord;
             }
             catch (ServiceException e)
             {
                 log.LogInformation("We could not retrieve the user's list: " + $"{e}");
             }
-
+            return null;
         }
-
 
         /// <summary>
         /// An example of how to authenticate the Microsoft Graph SDK using the MSAL library
@@ -254,41 +293,72 @@ namespace GetCallRecords_Http
         /// <param name="result">Object to display</param>
         private static void Display(JsonNode result)
         {
-            JsonArray nodes = ((result as JsonObject).ToArray()[1]).Value as JsonArray;
-
-            foreach (JsonObject aNode in nodes.ToArray())
+            if (result == null)
             {
-                foreach (var property in aNode.ToArray())
-                {
-                    Console.WriteLine($"{property.Key} = {property.Value?.ToString()}");
-                }
-                Console.WriteLine();
+                Console.WriteLine("The result content is null");
             }
-        }
-
-        /// <summary>
-        /// Checks if the sample is configured for using ClientSecret or Certificate. This method is just for the sake of this sample.
-        /// You won't need this verification in your production application since you will be authenticating in AAD using one mechanism only.
-        /// </summary>
-        /// <param name="config">Configuration from appsettings.json</param>
-        /// <returns></returns>
-        private static bool IsAppUsingClientSecret(AuthenticationConfig config)
-        {
-            string clientSecretPlaceholderValue = "[Enter here a client secret for your application]";
-
-            if (!String.IsNullOrWhiteSpace(config.ClientSecret) && config.ClientSecret != clientSecretPlaceholderValue)
-            {
-                return true;
-            }
-
-            else if (config.Certificate != null)
-            {
-                return false;
-            }
-
             else
-                throw new Exception("You must choose between using client secret or certificate. Please update appsettings.json file.");
+            {
+                JsonArray nodes = ((result as JsonObject).ToArray()[1]).Value as JsonArray;
+
+                foreach (JsonObject aNode in nodes.ToArray())
+                {
+                    foreach (var property in aNode.ToArray())
+                    {
+                        Console.WriteLine($"{property.Key} = {property.Value?.ToString()}");
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
+
+        ///// <summary>
+        ///// Checks if the sample is configured for using ClientSecret or Certificate. This method is just for the sake of this sample.
+        ///// You won't need this verification in your production application since you will be authenticating in AAD using one mechanism only.
+        ///// </summary>
+        ///// <param name="config">Configuration from appsettings.json</param>
+        ///// <returns></returns>
+        //private static bool IsAppUsingClientSecret(AuthenticationConfig config)
+        //{
+        //    string clientSecretPlaceholderValue = "[Enter here a client secret for your application]";
+
+        //    if (!String.IsNullOrWhiteSpace(config.ClientSecret) && config.ClientSecret != clientSecretPlaceholderValue)
+        //    {
+        //        return true;
+        //    }
+
+        //    else if (config.Certificate != null)
+        //    {
+        //        return false;
+        //    }
+
+        //    else
+        //        throw new Exception("You must choose between using client secret or certificate. Please update appsettings.json file.");
+        //}
+
+        public class SubscriptionData
+        {
+            public List<Value> value { get; set; }
+        }
+
+        public class Value
+        {
+            public string tenantId { get; set; }
+            public string subscriptionId { get; set; }
+            public string clientState { get; set; }
+            public string changeType { get; set; }
+            public string resource { get; set; }
+            public DateTime subscriptionExpirationDateTime { get; set; }
+            public ResourceData resourceData { get; set; }
+        }
+
+        public class ResourceData
+        {
+            public string oDataType { get; set; }
+            public string oDataId { get; set; }
+            public string id { get; set; }
+        }
+
     }
 }
 
