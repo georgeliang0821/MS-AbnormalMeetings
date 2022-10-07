0884063ec879fa6de2b65b9db837b23602fd9c85
Eric Lu <a-ericlu@microsoft.com>
2022-10-08 02:53:51 +0800

    Update: AssistTools/settingsToConfig.py, python tool to auto generare

--- AssistTools/settingsToConfig.py
+++ AssistTools/settingsToConfig.py
@@ -1,20 +1,41 @@
 import json
+import os
  
 # Opening JSON file
-json_path = '../GetCallRecords_Http/local.settings.json'
-originalConfig_path = './originalConfig.json'
+# json_path = '../src/AbnormalMeetings/local.settings.json'
+# originalConfig_path = './originalConfig.json'
+
+json_path = './src/AbnormalMeetings/local.settings.json'
+originalConfig_path = './AssistTools/originalConfig.json'
+newConfig_path = "./AssistTools/new_config.json"
+
+if not os.path.exists(json_path):
+    print(f"{json_path} does not exist. You may in the wrong folder.")
+    raise
+if not os.path.exists(originalConfig_path):
+    print(f"{originalConfig_path} does not exist. You may in the wrong folder.")
+    raise
+if not os.path.exists(newConfig_path):
+    print(f"{newConfig_path} does not exist. You may in the wrong folder.")
+    raise
 
 ignore_names = []
 # total_dict = {}
 new_config = []
 
 with open(originalConfig_path, 'r', encoding='utf-8-sig') as json_file:
-    data = json.loads(json_file.read())
+
+    json_file_read = json_file.read()
+    if (json_file_read[0] == '"'):
+        raise("You should paste your original configuration ")
+        
+    data = json.loads(json_file_read)
     new_config.extend(data)
     for a_data in new_config:
         ignore_names.append(a_data["name"])    
 
 with open(json_path, 'r', encoding='utf-8-sig') as json_file:
+
     data = json.loads(json_file.read())
  
     for k, v in data['Values'].items():
@@ -27,9 +48,9 @@ with open(json_path, 'r', encoding='utf-8-sig') as json_file:
         }
         new_config.append(tmp_dict)
 
-# print(type(new_config[0]))
-# print(ignore_names)
-with open("new_config.json", 'w') as f:
+with open(newConfig_path, 'w') as f:
     json_string = json.dumps(new_config)
+    print("You can now copy and paste the json string below to \"Advanced edit\".")
+    print()
     print(json_string)
     f.write(json_string)
